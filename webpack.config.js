399e6f3130276b1457a0e0f1dddb458b84ea9c93
chao.ma <chao.ma@lincomb.com>
2017-12-04 08:48:19 +0800

    'update-version'

--- webpack.config.js
+++ webpack.config.js
@@ -174,7 +174,7 @@ if (process.env.NODE_ENV === 'production') {
     // Banner
     var moment = require('moment');
     var pkg = require('./package.json');
-    var banner = 'Vue Scroller \nversion: ' + pkg.version + ' \nrepo: https://github.com/wangdahoo/vue-scroller \nbuild: ' + moment().format('YYYY-MM-DD HH:mm:ss')
+    var banner = 'Vue Simple Scroll \nversion: ' + pkg.version + ' \nrepo: https://github.com/machaosuper/vue-simple-scroll \nbuild: ' + moment().format('YYYY-MM-DD HH:mm:ss')
     module.exports.plugins.push(
       new webpack.BannerPlugin({ 
         banner: banner,
